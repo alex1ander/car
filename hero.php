27588127c5c7b56829868d6756af86472805dfc2
Alex1under <alexanderanufriev111@gmail.com>
2026-03-09 20:48:00 +0100

    Stop tracking node_modules

---
+++ hero.php
@@ -0,0 +1,24 @@
+<section class="scroll-snap-section">
+    <div class="hero-wrapper">
+        <img src="/assets/images/hero-bg.jpg" alt="" class="bg-image">
+
+        <div class="container">
+            <div class="line-titles-block">
+                <h2 class="line-title light-text">CarpowrapsCarpowraps</h2>
+            </div>
+
+
+            <div class="bottom-part">
+                <div class="text-place">
+                    <span class="short-description regular-text">
+                        Твоя машина — это твоя АВТОБИОГРАФИЯ
+                    </span>
+                </div>
+
+                <div class="button-place">
+                    <button class="glass-button">Забронируй сейчас</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
